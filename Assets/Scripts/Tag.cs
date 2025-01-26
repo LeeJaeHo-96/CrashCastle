b0e2b9bef471369dfdeb06fb25a01991ada80984
LeeJaeHo-96 <towpam123@gmail.com>
2025-01-26 13:17:15 +0900

    프로젝트 시작

---
+++ Assets/Scripts/Tag.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tag
+{
+    public static string Untagged = "Untagged";
+    public static string Respawn = "Respawn";
+    public static string Finish = "Finish";
+    public static string EditorOnly = "EditorOnly";
+    public static string MainCamera = "MainCamera";
+    public static string Player = "Player";
+    public static string GameController = "GameController";
+    public static string Enemy = "Enemy";
+}
